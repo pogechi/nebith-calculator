bed9aa9d5031240fd0532ccbfeb36b0e68ac133c
pogechi <84137850+pogechi@users.noreply.github.com>
2026-02-05 19:06:08 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,8 +120,8 @@ if generate:
             "Email": [email_input],
             "Date": [today_iso]
         })
-        conn.write(form_data, sheet="Form Responses", mode="append")
-        
+        conn.update(data=form_data, sheet="Form Responses", mode="append")
+
         st.success(f"Thank you {name_input}! Your report is being generated and will be sent to your e-mail address shortly. Please find below a preview of the results.")
 
     # Locate city
